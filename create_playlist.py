117d6014b4b20957c32f2cc3e4f2f31d975102ea
cahmarti <cahmarti@cahmarti-t480.us.oracle.com>
2020-02-19 22:43:56 -0600

    Initial commit

---
+++ create_playlist.py
@@ -0,0 +1,32 @@
+# Step 1: Log Into Youtube
+# Step 2: Grab Our Liked Videos
+# Step 3: Create New Playlist
+# Step 4: Search For Song
+# Step 5: Add Song Into New Spotify Playlist
+
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    # Step 1: Log Into Youtube
+    def get_youtube_client(self):
+        pass
+
+    # Step 2: Grab Our Liked Videos
+    def get_liked_videos(self):
+        pass
+
+    # Step 3: Create New Playlist
+    def create_playlist(self):
+        pass
+
+    # Step 4: Search For Song
+    def get_spotify_url(self):
+        pass
+
+    # Step 5: Add This Song Into New Spotify Playlist
+    def add_song_to_playlist(self):
+        pass
+
